83931900271a5eeb2973ac45c04fe416de0c1355
Paulkm2006 <paulkm@vip.qq.com>
2021-01-21 22:48:34 +0800

    Update addnat.sh

--- addnat.sh
+++ addnat.sh
@@ -7,14 +7,17 @@ for d in $(seq 1 50);do
 		ssh_port="6100"${d}
 		user_port_first="100"${d}"0"
 		user_port_last="100"${d}"9"
+		echo user_port_last
 	elif (("$d" < 100)); then
 		ssh_port="610"${d}
 		user_port_first="10"${d}"0"
 		user_port_last="10"${d}"9"
+		echo user_port_last
 	else
 		ssh_port="61"${d}
 		user_port_first="1"${d}"0"
 		user_port_last="1"${d}"9"
+		echo user_port_last
 	fi
 	iptables -t nat -A PREROUTING -i vmbr0 -p tcp -m tcp --dport ${ssh_port} -j DNAT --to-destination ${user_ip}:22
 	iptables -t nat -A PREROUTING -i vmbr0 -p tcp -m tcp --dport ${user_port_first}:${user_port_last} -j DNAT --to-destination ${user_ip}
