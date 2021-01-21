2f53cfd699c0d70035d8fe532754e8647b966592
Paulkm2006 <paulkm@vip.qq.com>
2021-01-21 23:00:48 +0800

    Update addnat.sh

--- addnat.sh
+++ addnat.sh
@@ -17,5 +17,5 @@ for ((d=1; d<=50; d++)); do
 	iptables -t nat -A PREROUTING -i vmbr0 -p tcp -m tcp --dport ${ssh_port} -j DNAT --to-destination ${user_ip}:22
 	iptables -t nat -A PREROUTING -i vmbr0 -p tcp -m tcp --dport ${user_port_first}:${user_port_last} -j DNAT --to-destination ${user_ip}
 	iptables -t nat -A PREROUTING -i vmbr0 -p udp -m udp --dport ${user_port_first}:${user_port_last} -j DNAT --to-destination ${user_ip}	
-; done
+done
 iptables-save > /etc/iptables/rules.v4
